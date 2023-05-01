6c807a97054862b8e390d537ec452f2b8cca6cf9
rui-1017 <rui1017t@gmail.com>
2023-05-01 20:57:39 +0900

    1-4

--- JavaStudy/1-4/Task1_4.java
+++ JavaStudy/1-4/Task1_4.java
@@ -41,28 +41,28 @@ public class Task1_4 {
         //      定数を使用して「 ログイン成功です 」と出力してください。
 
        if (name.equals(USER_NAME) && pass.equals(USER_PASSWORD)) {
-    System.out.println("ログイン成功です");
+    System.out.println(CONST_MSG_SUCCESS);
        }
 
 
         // ③ 「USER_NAME」の値のみ等しい場合。
        //     定数を使用して「 パスワードに誤りがあります。 」 と出力してください。
        else if (name.equals(USER_NAME) && !(pass.equals(USER_PASSWORD))) {
-    System.out.println("パスワードに誤りがあります");
+    System.out.println(CONST_MSG_ERROR_PASS);
        }
 
 
         // ④ 「USER_PASSWORD」の値のみ等しい場合。
        //定数を使用して「 名前に誤りがあります。 」と出力してください。
        else if ((name.equals(USER_PASSWORD)) && !(pass.equals(USER_NAME))) {
-    System.out.println("名前に誤りがあります");
+    System.out.println(CONST_MSG_ERROR_NAME);
        }
 
 
         // ⑤ 「USER_NAME」も「USER_PASSWORD」の値も間違っていた場合。 
        //定数を使用して「 入力情報に誤りがあります。 」と出力してください。
          else if (!(name.equals(USER_NAME)) && !(pass.equals(USER_PASSWORD)) ){
-    System.out.println("入力情報に誤りがあります");
+    System.out.println(CONST_MSG_ERROR_INPUT);
        }
 
 
