bd6ee483fe5cd6d58105b1d03531f502ad7bd148
rui-1017 <rui1017t@gmail.com>
2023-07-13 23:05:15 +0900

    c

---
+++ JavaStudy/2-3/src/study/Task.java
@@ -0,0 +1,19 @@
+	package study;
+	
+public class Task extends Calculator {
+
+	// ① TaskクラスにCalculatorクラスを継承させなさい。
+	    /**
+	     * タスクの実行
+	     */
+	    public void doTask() {
+
+	        // ② Calculator.javaのすべてのオーバーロードメソッド「plus」に適当な引数を与え、下記画像のよう出力されるようコーディングしなさい。
+	        // 尚、「どのクラスから呼び出しているか」を明確にするため、plus()には呼び出し元のキーワードを付与すること。
+	      
+	       Calculator cal = new Calculator();
+	        System.out.println("plusメソッドの引数が1つの場合：" + cal.plus(10));
+	        System.out.println("plusメソッドの引数が2つの場合：" + cal.plus(10,20));
+	        System.out.println("plusメソッドの引数が3つの場合：" + cal.plus(10,20,70));
+	    }
+}
